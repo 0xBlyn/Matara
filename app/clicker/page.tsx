60a358aa6268e1622271fbc9196c933b79fbc666
0xBlyn <blyneze@gmail.com>
2024-12-09 14:03:16 +0100

    main

--- app/clicker/page.tsx
+++ app/clicker/page.tsx
@@ -1,49 +1,37 @@
-'use client'
+// app/clicker/page.tsx
 
-import React, { useState, useEffect, useCallback, ReactNode } from 'react';
-import { createGameStore, InitialGameState } from '@/utils/game-mechaincs';
+import React, { useState, useEffect, useCallback } from 'react';
+import { ToastContainer } from 'react-toastify';
+import LoadingScreen from '@/components/Loading';
 import Game from '@/components/Game';
 import Mine from '@/components/Mine';
 import Friends from '@/components/Friends';
 import Earn from '@/components/Earn';
 import Airdrop from '@/components/Airdrop';
-import Navigation from '@/components/Navigation';
-import LoadingScreen from '@/components/Loading';
-import { energyUpgradeBaseBenefit } from '@/utils/consts';
 import Boost from '@/components/Boost';
-import { ToastContainer } from 'react-toastify';
+import Navigation from '@/components/Navigation';
 import { AutoIncrement } from '@/components/AutoIncrement';
 import { PointSynchronizer } from '@/components/PointSynchronizer';
-import TopInfoSection from '@/components/TopInfoSection';
 
-function ClickerPage() {
+const ClickerPage: React.FC = () => {
     const [currentView, setCurrentViewState] = useState<string>('loading');
     const [isInitialized, setIsInitialized] = useState(false);
 
     const setCurrentView = useCallback((newView: string) => {
         console.log('Changing view to:', newView);
         setCurrentViewState(newView);
-    }, [setCurrentViewState]);
+    }, []);
 
     const renderCurrentView = useCallback(() => {
         if (!isInitialized) {
-            return <LoadingScreen
-                setIsInitialized={setIsInitialized}
-                setCurrentView={setCurrentView}
-            />;
+            return <LoadingScreen setIsInitialized={setIsInitialized} setCurrentView={setCurrentView} />;
         }
 
         switch (currentView) {
             case 'game':
-                return <Game
-                    currentView={currentView}
-                    setCurrentView={setCurrentView}
-                />;
+                return <Game currentView={currentView} setCurrentView={setCurrentView} />;
             case 'boost':
-                return <Boost
-                    currentView={currentView}
-                    setCurrentView={setCurrentView}
-                />
+                return <Boost currentView={currentView} setCurrentView={setCurrentView} />;
             case 'mine':
                 return <Mine />;
             case 'friends':
@@ -53,40 +41,25 @@ function ClickerPage() {
             case 'airdrop':
                 return <Airdrop />;
             default:
-                return <Game
-                    currentView={currentView}
-                    setCurrentView={setCurrentView}
-                />;
+                return <Game currentView={currentView} setCurrentView={setCurrentView} />;
         }
     }, [currentView, isInitialized]);
-
-    console.log('ClickerPage rendering. Current state:', { currentView, isInitialized });
-
-    // Example of checking for window before using it
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            // Any code that requires window can go here
-            console.log('Window is defined, running client-side code.');
-        }
-    }, []);
 
     return (
         <div className="min-h-screen text-white">
             <ToastContainer />
-            {
-                isInitialized &&
+            {isInitialized && (
                 <>
                     <AutoIncrement />
                     <PointSynchronizer />
                 </>
-            }
+            )}
             {renderCurrentView()}
             {isInitialized && currentView !== 'loading' && (
-                <Navigation
-                    currentView={currentView}
-                    setCurrentView={setCurrentView}
-                />
+                <Navigation currentView={currentView} setCurrentView={setCurrentView} />
             )}
         </div>
     );
-}
+};
+
+export default ClickerPage;
